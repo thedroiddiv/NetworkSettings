9bdf74b8dcc8c2b63b867c192d806fc11ed85dcf
Divyansh Kushwaha <thedroiddiv@gmail.com>
2023-11-30 20:16:31 +0530

    chore: create app

---
+++ app/build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.thedroiddiv.networksettings"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.thedroiddiv.networksettings"
+        minSdk = 22
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.3"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    implementation("androidx.activity:activity-compose:1.8.1")
+}
---
+++ app/src/main/java/com/thedroiddiv/networksettings/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.thedroiddiv.networksettings
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        try {
+            val intent = Intent("android.intent.action.MAIN")
+            intent.setClassName("com.android.phone", "com.android.phone.settings.RadioInfo")
+            startActivity(intent)
+        } catch (e: Exception) {
+            Toast.makeText(applicationContext, "Device not supported", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+    }
+}
